d16c424d66e05908e99da564c097fd0ebfc05596
glucose6126 <gusvy1234@gmail.com>
2016-11-17 01:05:06 +0900

    End

--- Note.py
+++ Note.py
@@ -3,23 +3,30 @@ from PIL import ImageFont
 from PIL import ImageDraw
 import ctypes
 import re
+import os
 
 def setting() :
-    setting = re.findall('[(.+?)]', open('set.ini').read())
-    return setting
+    setting = re.findall('\[(.+?)\]', open('set.ini').read())
+    font    = setting[0]
+    size    = int(setting[1])
+    backimg = setting[2]
+    x       = int(setting[3])
+    y       = int(setting[4])
+    space   = int(setting[5])
+    return font, size, backimg, x, y, space
 
 font, size, backimg, x, y, space = setting()
-notes = open('note.txt', 'rb').read().split('\r\n')
+
+notes = open('note.txt', 'rb').read()[3:].split('\r\n')
 image = Image.open(backimg)
 
 draw = ImageDraw.Draw(image)
-font = ImageFont.truetype(font, int(size))
+font = ImageFont.truetype(font, int(size), encoding='utf-8')
 
 for note in notes :
-    draw.text((x, y), note, (255,255,255), font=font)
+    draw.text((x, y), note.decode('utf-8'), (255,255,255), font=font)
     y += space
 
 image.save('bg.png')
-
-SPI_SETDESKWALLPAPER = 20
-ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, "image.jpg" , 0)
+path = os.getcwd()
+ctypes.windll.user32.SystemParametersInfoA(20, 0, path + "\\bg.png" , 0)
